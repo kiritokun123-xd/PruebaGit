8cd8960d8ea93078b3ed30727ca1138371d134ed
kiritokun123-xd <david22android@gmail.com>
2022-08-07 12:15:25 -0500

    Saludo en ejercicio en Main

--- Matrices/src/main/java/EjercioNuevoMain.java
+++ Matrices/src/main/java/EjercioNuevoMain.java
@@ -2,4 +2,7 @@ public class EjercioNuevoMain {
     String saludo = "Se modifica el ejercicio en Main";
     //error en ejercicio, se soluciona
     int total = 1000;
+
+    //hola mundo
+    String hola = "hola mundo";
 }
